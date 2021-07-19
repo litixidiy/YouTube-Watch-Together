eb2ce1e2b0de1b3b2a69042ec8acfe6377d6ac55
Ryan Litixidiy <lch.ryanliu@gmail.com>
2021-07-18 22:38:40 -0400

    added general implementation

--- server/server.py
+++ server/server.py
@@ -1,44 +1,54 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send, emit,join_room, leave_room, \
-close_room, rooms, disconnect
-
+from flask_socketio import SocketIO, send, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
 import config
 
-#Creating an instance of Flask
+'''
+API 
+Namespace:
+time - recieves the time and make adjustment if needed
+
+
+connect : is triggered when the client is successfully connected to the server
+message : is triggered when the client sends data
+disconnect : is triggered when the client loses the connection with the server, 
+    -closing the browser, dropping the connection, etc.
+
+'''
+
+
+# Creating an instance of Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config.Config.SECRET_KEY
 
-socketio = SocketIO(app,logger=True, engineio_logger=True,cors_allowed_origins='*')
-print("[Server Started] : " +config.Config.ADRESS )
+socketio = SocketIO(app, logger=True, engineio_logger=True,
+                    cors_allowed_origins='*')
+print("[Server Started] : " + config.Config.ADRESS)
 
-#Communication Functions To The Client
+# Communication Functions To The Client
+
+
 @socketio.on('connect')
 def test_connect():
     print('[Client Connected]')
     socketio.emit("Connected")
 
+
 @socketio.on('disconnect')
 def test_disconnect():
     print('[Client Disconnected]')
 
+
 @socketio.on('event')
-def handle_message(json, methods=['GET', 'POST']):
-    """
-    handles saving messages once received from web server
-    and sending message to other clients
-    :param json: json
-    :param methods: POST GET
-    :return: None
-    """
+def handle_message(json):
+
+
     data = dict(json)
     if "time" in data:
         print(data["time"])
-        '''
-        db = DataBase()
-        db.save_message(data["name"], data["message"])'''
 
     socketio.emit('message response', json)
 
 
 if __name__ == '__main__':
-    socketio.run(app,debug=config.Config.DEBUG)
+    socketio.run(app, debug=config.Config.DEBUG)
